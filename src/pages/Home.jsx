de80c6117ae8d23c917ff26817b841504a9d5c63
kingjethro999 <jethrojerrybj@gmail.com>
2026-02-23 13:59:38 +0100

    fix(ui): adjust home container height for absolute centering

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import { SwipeFeed } from "../components/swipe-feed"
 
 export default function Home() {
   return (
-    <div className="flex-1 w-full h-full relative">
-      <main className="flex-1 overflow-hidden relative">
+    <div className="flex-1 w-full relative">
+      <main className="flex-1 min-h-[calc(100vh-64px)] overflow-hidden relative border-none">
         <SwipeFeed />
       </main>
-      </div>
+    </div>
   )
 }
